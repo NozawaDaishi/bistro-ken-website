434a72d7fb8ec3bee496cd18e81aad38ca22a4d2
NozawaDaishi <nozawa171040@gmail.com>
2023-11-06 23:59:03 +0900

    add: piniaのsetup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import './style.css'
 import App from './App.vue'
 import i18n from '@/i18n'
 import router from './router'
+import { setupPinia } from './stores'
 
 const app = createApp(App)
+const pinia = setupPinia()
 
 app.use(i18n)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,15 @@
 import { createPinia } from 'pinia'
+import { markRaw } from 'vue'
+import router from '@/router'
 
 // Piniaインスタンスを作成する関数をエクスポート
 export const setupPinia = () => {
   const pinia = createPinia()
+
+  // Piniaプラグインとしてrouterを追加
+  pinia.use(({ store }) => {
+    store.router = markRaw(router)
+  })
+
   return pinia
 }
