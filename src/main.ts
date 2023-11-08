14899796d05734dd982dd071852f4edf0dee811b
NozawaDaishi <nozawa171040@gmail.com>
2023-11-08 21:19:03 +0900

    add: faviconなどの素材、reset.scssの追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import '@/global.scss'
+import App from './App.vue'
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 import i18n from '@/i18n'
 import router from './router'
 import { setupPinia } from './stores'
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,15 +1,8 @@
 import { createPinia } from 'pinia'
-import { markRaw } from 'vue'
-import router from '@/router'
 
 // Piniaインスタンスを作成する関数をエクスポート
 export const setupPinia = () => {
   const pinia = createPinia()
-
-  // Piniaプラグインとしてrouterを追加
-  pinia.use(({ store }) => {
-    store.router = markRaw(router)
-  })
 
   return pinia
 }
